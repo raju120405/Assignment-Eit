c1a369f5486e43fee34ec261839a67c7975f6c15
raju120405 <raju120405@gmail.com>
2022-11-10 21:27:12 +0600

    fill

--- Assignment.js
+++ Assignment.js
@@ -1,5 +1,13 @@
-// concat 
+             // concat 
+/*
 const firstWord = ['We Are '];
 const EndWord = [ 'The Best Friend'];
 const AddWord = firstWord.concat(   EndWord);
 console.log(AddWord);
+*/
+               //fill
+/*
+const friends = ['Raju','Tuhin','shipon'];
+friends.fill('Arafat');
+console.log(friends)
+*/
