558afdadd26c5dc375374a4acaeea97e2f86f884
raju120405 <raju120405@gmail.com>
2022-11-11 11:47:04 +0600

    find-filter

--- Assignment.js
+++ Assignment.js
@@ -11,3 +11,21 @@ const friends = ['Raju','Tuhin','shipon'];
 friends.fill('Arafat');
 console.log(friends)
 */
+
+             // find
+/*
+const friendList = [
+    {name:'raju',age:22},
+    {name:'tuhin',age:20},
+    {name:'shipon',age:21},
+];
+const oldestFriend = friendList.find(friend => friend.age == 22);
+console.log(oldestFriend);
+
+*/
+            //   filter
+/*          
+const friendAge = [17,20,18,19,23];
+const seniorFriend = friendAge.filter(Number => Number > 20);
+console.log(seniorFriend);
+*/
