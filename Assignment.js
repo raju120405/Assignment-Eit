3d0b1b5fd64430ba677820ffb7544c60e3eac738
raju120405 <raju120405@gmail.com>
2022-11-15 13:10:15 +0600

    push pop join

--- Assignment.js
+++ Assignment.js
@@ -41,9 +41,57 @@ console.log(seniorFriend);
 
 // map()
 //  const products = [
-//     {name:'phone', brand:'Realme', price: 25000,},
-//     {name:'camera', brand:'Canon', price: 55000,},
-//     {name:'laptop', brand:'Hp', price: 25000,}
+//     {name:'phone', brand:'Samsung', price: 25000},
+//     {name:'camera', brand:'Canon', price: 55000},
+//     {name:'laptop', brand:'Hp', price: 25000}
 //  ];
 //  const brand = products.map(product => product.brand);
 //  console.log(brand);
+
+// push
+// const friends = ['bmc','arafat','rubel','sobuj'];
+// friends.push('tuhin');
+// console.log(friends);
+
+// pop
+// const friends = ['bmc', 'arafat', 'rubel', 'sobuj', 'tuhin'];
+// friends.pop('tuhin');
+// console.log(friends);
+
+// shift
+// const friends = ['bmc','arafat','rubel','sobuj'];
+// friends.shift()
+// console.log(friends);
+
+// unshift
+// const friends = ['bmc','arafat','rubel','sobuj'];
+// friends.unshift('adnan');
+// console.log(friends);
+
+// join
+// const books = [ 'bangla','english','math'];
+// const newBooks = books.join(' and ');
+// console.log(newBooks);
+
+// includes
+// const books = [ 'bangla', 'english', 'math'];
+// const addBooks = books.includes('math');
+// console.log(addBooks);
+
+// reverse
+// const books = [ 'bangla','english','physic'];
+// books.reverse();
+// console.log(books);
+
+// slice
+// const country =['bangladesh','india', 'pakistan','usa','argentina'];
+// const removed = country.slice(1);
+// console.log(removed);
+
+// Write a program for concatenation two array (using spread operator).
+
+// const Numbers = [12,04,05,56];
+// const max = Math.max(32,44,56);
+// const maxNumber = Math.max(...Numbers)
+// console.log(max , maxNumber);
+
